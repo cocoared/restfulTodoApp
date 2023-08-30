2a333d5645a5965b4abaac03a0fc9832fa7a4039
tkyy22 <yoshino.takaya@itfllc.co.jp>
2023-08-30 16:56:00 +0900

    初回コミット

---
+++ src/main/java/com/example/demo/RestTodoAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestTodoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestTodoAppApplication.class, args);
+	}
+
+}
